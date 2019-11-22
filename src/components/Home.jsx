1c74d7fe529d04e0279106be511f28701547e151
Micah Zaayer <micahzaayer@gmail.com>
2019-11-22 11:43:19 -0800

    add more styling to navbar.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,12 +14,6 @@ function Home() {
   };
 
   var imgStyle = {
-    // margin: '10% auto',
-    // display: 'block',
-    // position: 'relative',
-    // bottom: '20%',
-    // right: '20%',
-    // borderRadius: '1%',
     zIndex: '1',
   };
 
@@ -34,12 +28,8 @@ function Home() {
     textShadow: '4px 4px 0px #d5d5d5, 7px 7px 0px rgba(0, 0, 0, 0.2)',
     position: 'fixed',
     textAlign: 'center',
-    // left: '50%',
-    top: '50%'
-    // padding: '80px 50px',
-    // top: '50%', 
-    // left: '50%', 
-    // transform: 'translate(-50%, -50%)'
+    borderStyle: 'solid',
+    borderColor: 'white'
   };
   return (
     <div style={siteStyle} className="container">
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import bottle from '../Assets/Img/bottle.png';
 
 function NavBar() {
 
   var navStyle = {
     paddingLeft: '2%',
     paddingRight: '2%',
-    backgroundColor: 'blue',
+    backgroundColor: 'black',
     fontFamily: 'Tomorrow, sans-serif',
     fontWeight: 'bold',
-    borderRadius: '10px'
+    borderRadius: '5px',
+    width: "100%",
+    marginBottom: '30px'
+  };
 
+  var bottleStyle = {
+    height: '75px'
   };
 
   return (
     <nav style={navStyle}>
       <div className="nav-wrapper">
-        <span><Link to="/"><i className="fas fa-beer fa-4x"></i></Link></span>
+        <span><Link to="/"><img src={bottle} style={bottleStyle}/></Link></span>
         <ul id="nav-mobile" className="right">
           <li><Link to="/Kombucha">Drafts</Link></li>
         </ul>
